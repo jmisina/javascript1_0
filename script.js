0833a82cc68f2c8d47cb04f5582ffd6cff331adb
Jakub Misina <109918766+jmisina@users.noreply.github.com>
2024-10-02 09:43:59 +0000

    ex1

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 (function () {
-  //TODO
+document.getElementById('ex1_button').addEventListener('click', function() {
+const numbers = Array.from({ length: 10 }, (_, i) => i).join(', ');
+document.getElementById('ex1_content').textContent = numbers;
+});
+                       
+                       
+  
 })();
